b774a7dcbf4e6ccbb307ded1890b864027d991c1
ArmandoBGarcia <xdarcx@hotmail.es>
2022-09-28 11:57:52 -0500

    mergin routes configuration

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,28 +1,16 @@
-<<<<<<< HEAD
-const { Router } = require("express");
 require("dotenv").config();
+const { Router } = require("express");
 const router = Router();
+const anime = require("./animes.ts");
+const genre = require("./genres.ts");
 import manga from "./manga.routes";
-=======
-const { Router } = require('express');
-const anime= require("./animes.ts")
-const genre= require("./genres.ts")
-
-require('dotenv').config()
-
-
-
-
-
-const router = Router();
-router.use("/animes",anime)
-router.use("/genres",genre)
->>>>>>> e4131a0e3044a2574eb72084e622aa3385fd72bd
 
 router.get("/", (req: any, res: { send: (arg0: string) => void }) => {
   res.send("Hello world");
 });
 
 router.use("/manga", manga);
+router.use("/animes", anime);
+router.use("/genres", genre);
 
 export default router;
