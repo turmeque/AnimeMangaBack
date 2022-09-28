adb9f60d0b3f5aa861a775aa854c8c797fa6269c
ArmandoBGarcia <xdarcx@hotmail.es>
2022-09-28 11:57:00 -0500

    search in api or bring to the DB

--- src/controller/controllerManga.ts
+++ src/controller/controllerManga.ts
@@ -3,162 +3,165 @@ import axios from "axios";
 import db from "../../models";
 
 export const getMangas = async () => {
-  let allMangas = [];
-  const url = "https://api.jikan.moe/v4/manga/";
-  const url2 = "https://api.jikan.moe/v4/manga?page=2";
-  const url3 = "https://api.jikan.moe/v4/manga?page=3";
-  const url4 = "https://api.jikan.moe/v4/manga?page=4";
+  // let allMangas = [];
+  const mangasDb = await db.Manga.findAll();
+  if (!mangasDb.length) {
+    const url = "https://api.jikan.moe/v4/manga/";
+    const url2 = "https://api.jikan.moe/v4/manga?page=2";
+    const url3 = "https://api.jikan.moe/v4/manga?page=3";
+    const url4 = "https://api.jikan.moe/v4/manga?page=4";
 
-  const response = await axios.get(url);
-  const response2 = await axios.get(url2);
-  const response3 = await axios.get(url3);
-  const response4 = await axios.get(url4);
+    const response = await axios.get(url);
+    const response2 = await axios.get(url2);
+    const response3 = await axios.get(url3);
+    const response4 = await axios.get(url4);
 
-  const callOne = response.data.data.map(
-    (d: {
-      genres: any;
-      mal_id: any;
-      synopsis: any;
-      status: any;
-      chapters: any;
-      popularity: any;
-      score: any;
-      images: any;
-      titles: {
-        filter(arg0: (d: any) => boolean): any;
-        map(arg0: (d: any) => any): unknown;
-        title: any;
-      };
-    }) => {
-      return {
-        id: d.mal_id,
-        title: d.titles
-          .filter((d) => d.type === "Default")
-          .map((d: { title: any }) => d.title)
-          .join(" "),
-        image: d.images.jpg.large_image_url,
-        score: d.score,
-        popularity: d.popularity,
-        chapters: d.chapters,
-        status: d.status,
-        synopsis: d.synopsis,
-        genres: d.genres
-          .map((d: { name: any; type: any }) => d.name)
-          .join(", "),
-      };
-    }
-  );
+    const callOne = response.data.data.map(
+      (d: {
+        genres: any;
+        mal_id: any;
+        synopsis: any;
+        status: any;
+        chapters: any;
+        popularity: any;
+        score: any;
+        images: any;
+        titles: {
+          filter(arg0: (d: any) => boolean): any;
+          map(arg0: (d: any) => any): unknown;
+          title: any;
+        };
+      }) => {
+        return {
+          id: d.mal_id,
+          title: d.titles
+            .filter((d) => d.type === "Default")
+            .map((d: { title: any }) => d.title)
+            .join(" "),
+          image: d.images.jpg.large_image_url,
+          score: d.score,
+          popularity: d.popularity,
+          chapters: d.chapters,
+          status: d.status,
+          synopsis: d.synopsis,
+          genres: d.genres
+            .map((d: { name: any; type: any }) => d.name)
+            .join(", "),
+        };
+      }
+    );
 
-  const callTwo = response2.data.data.map(
-    (d: {
-      genres: any;
-      mal_id: any;
-      synopsis: any;
-      status: any;
-      chapters: any;
-      popularity: any;
-      score: any;
-      images: any;
-      titles: {
-        filter(arg0: (d: any) => boolean): any;
-        map(arg0: (d: any) => any): unknown;
-        title: any;
-      };
-    }) => {
-      return {
-        id: d.mal_id,
-        title: d.titles
-          .filter((d) => d.type === "Default")
-          .map((d: { title: any }) => d.title)
-          .join(" "),
-        image: d.images.jpg.large_image_url,
-        score: d.score,
-        popularity: d.popularity,
-        chapters: d.chapters,
-        status: d.status,
-        synopsis: d.synopsis,
-        genres: d.genres
-          .map((d: { name: any; type: any }) => d.name)
-          .join(", "),
-      };
-    }
-  );
+    const callTwo = response2.data.data.map(
+      (d: {
+        genres: any;
+        mal_id: any;
+        synopsis: any;
+        status: any;
+        chapters: any;
+        popularity: any;
+        score: any;
+        images: any;
+        titles: {
+          filter(arg0: (d: any) => boolean): any;
+          map(arg0: (d: any) => any): unknown;
+          title: any;
+        };
+      }) => {
+        return {
+          id: d.mal_id,
+          title: d.titles
+            .filter((d) => d.type === "Default")
+            .map((d: { title: any }) => d.title)
+            .join(" "),
+          image: d.images.jpg.large_image_url,
+          score: d.score,
+          popularity: d.popularity,
+          chapters: d.chapters,
+          status: d.status,
+          synopsis: d.synopsis,
+          genres: d.genres
+            .map((d: { name: any; type: any }) => d.name)
+            .join(", "),
+        };
+      }
+    );
 
-  const callTree = response3.data.data.map(
-    (d: {
-      genres: any;
-      mal_id: any;
-      synopsis: any;
-      status: any;
-      chapters: any;
-      popularity: any;
-      score: any;
-      images: any;
-      titles: {
-        filter(arg0: (d: any) => boolean): any;
-        map(arg0: (d: any) => any): unknown;
-        title: any;
-      };
-    }) => {
-      return {
-        id: d.mal_id,
-        title: d.titles
-          .filter((d) => d.type === "Default")
-          .map((d: { title: any }) => d.title)
-          .join(" "),
-        image: d.images.jpg.large_image_url,
-        score: d.score,
-        popularity: d.popularity,
-        chapters: d.chapters,
-        status: d.status,
-        synopsis: d.synopsis,
-        genres: d.genres
-          .map((d: { name: any; type: any }) => d.name)
-          .join(", "),
-      };
-    }
-  );
+    const callTree = response3.data.data.map(
+      (d: {
+        genres: any;
+        mal_id: any;
+        synopsis: any;
+        status: any;
+        chapters: any;
+        popularity: any;
+        score: any;
+        images: any;
+        titles: {
+          filter(arg0: (d: any) => boolean): any;
+          map(arg0: (d: any) => any): unknown;
+          title: any;
+        };
+      }) => {
+        return {
+          id: d.mal_id,
+          title: d.titles
+            .filter((d) => d.type === "Default")
+            .map((d: { title: any }) => d.title)
+            .join(" "),
+          image: d.images.jpg.large_image_url,
+          score: d.score,
+          popularity: d.popularity,
+          chapters: d.chapters,
+          status: d.status,
+          synopsis: d.synopsis,
+          genres: d.genres
+            .map((d: { name: any; type: any }) => d.name)
+            .join(", "),
+        };
+      }
+    );
 
-  const callFour = response4.data.data.map(
-    (d: {
-      genres: any;
-      mal_id: any;
-      synopsis: any;
-      status: any;
-      chapters: any;
-      popularity: any;
-      score: any;
-      images: any;
-      titles: {
-        filter(arg0: (d: any) => boolean): any;
-        map(arg0: (d: any) => any): unknown;
-        title: any;
-      };
-    }) => {
-      return {
-        id: d.mal_id,
-        title: d.titles
-          .filter((d) => d.type === "Default")
-          .map((d: { title: any }) => d.title)
-          .join(" "),
-        image: d.images.jpg.large_image_url,
-        score: d.score,
-        popularity: d.popularity,
-        chapters: d.chapters,
-        status: d.status,
-        synopsis: d.synopsis,
-        genres: d.genres
-          .map((d: { name: any; type: any }) => d.name)
-          .join(", "),
-      };
-    }
-  );
+    const callFour = response4.data.data.map(
+      (d: {
+        genres: any;
+        mal_id: any;
+        synopsis: any;
+        status: any;
+        chapters: any;
+        popularity: any;
+        score: any;
+        images: any;
+        titles: {
+          filter(arg0: (d: any) => boolean): any;
+          map(arg0: (d: any) => any): unknown;
+          title: any;
+        };
+      }) => {
+        return {
+          id: d.mal_id,
+          title: d.titles
+            .filter((d) => d.type === "Default")
+            .map((d: { title: any }) => d.title)
+            .join(" "),
+          image: d.images.jpg.large_image_url,
+          score: d.score,
+          popularity: d.popularity,
+          chapters: d.chapters,
+          status: d.status,
+          synopsis: d.synopsis,
+          genres: d.genres
+            .map((d: { name: any; type: any }) => d.name)
+            .join(", "),
+        };
+      }
+    );
 
-  allMangas = [...callOne, ...callTwo, ...callTree, ...callFour];
+    let allMangas = [...callOne, ...callTwo, ...callTree, ...callFour];
 
-  await db.Manga.bulkCreate(allMangas);
+    await db.Manga.bulkCreate(allMangas);
 
-  return allMangas;
+    return allMangas;
+  } else return mangasDb;
 };
 
 export const getMangaGenres = async () => {
