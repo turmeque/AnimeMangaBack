bd414595a963a846a4873a7d08826c3142e9c8c1
ArmandoBGarcia <xdarcx@hotmail.es>
2022-09-28 20:41:08 -0500

    model of GenreMangas updated

--- models/GenresManga.ts
+++ models/GenresManga.ts
@@ -1,19 +1,40 @@
 "use strict";
-const { Model } = require("sequelize");
+import { Model } from "sequelize";
+
+interface GenresAttributes {
+  id: number;
+  name: string;
+}
+
 module.exports = (sequelize: any, DataTypes: any) => {
-  class GenresManga extends Model {
+  class GenresManga
+    extends Model<GenresAttributes>
+    implements GenresAttributes
+  {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
+    id!: number;
+    name!: string;
     static associate(models: any) {
       // define association here
     }
   }
   GenresManga.init(
     {
-      name: DataTypes.STRING,
+      id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+      },
     },
     {
       sequelize,
