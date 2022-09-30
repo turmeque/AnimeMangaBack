f458436e657954eca2486dd945f0718ced3486d6
ArmandoBGarcia <xdarcx@hotmail.es>
2022-09-30 10:48:11 -0500

    route users added

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,12 +4,14 @@ const router = Router();
 const anime = require("./animes.ts");
 const genre = require("./genres.ts");
 import manga from "./manga.routes";
+import users from "./user.routes";
 
 router.get("/", (req: any, res: { send: (arg0: string) => void }) => {
   res.send("Hello world");
 });
 
 router.use("/manga", manga);
+router.use("/users", users);
 router.use("/animes", anime);
 router.use("/genres", genre);
 
