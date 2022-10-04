6e58b533d9dbc6707ea49b26e4189f08421b699d
ArmandoBGarcia <xdarcx@hotmail.es>
2022-10-04 11:04:49 -0500

    new features added to the controller users

--- src/controller/controllerUsers.ts
+++ src/controller/controllerUsers.ts
@@ -1,19 +1,41 @@
 require("dotenv").config();
-import axios from "axios";
 import db from "../../models";
-import { users } from "../../seeders/users";
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const saltRounds = Number(process.env.SALT_ROUNDS);
+const secret = process.env.SECRET_WORD;
+const salt = bcrypt.genSaltSync(saltRounds);
 
-export const createUser = async (obj: any) => {
-  const { username, email, password, cellphone } = obj;
-  if (!username || !email || !password || !cellphone) {
+export const signUp = async (obj: any) => {
+  const { username, email, pass, cellphone } = obj;
+  if (!username || !email || !pass || !cellphone) {
     throw "Missing data require to create a new user";
   }
-  const profile = await db.Profile.create({ username });
-  const profile_id = profile.id;
-  // console.log(profile_id);
-  const newUser = await db.Users.create(obj);
-  newUser.setProfile(profile_id);
-  return "User created succesfully";
+
+  const password = bcrypt.hashSync(pass, salt);
+
+  const user = await db.Users.create({ username, email, password, cellphone });
+
+  const token = jwt.sign({ user }, secret, { expiresIn: "1hr" });
+
+  return { user, token };
+};
+
+export const signIn = async (obj: any) => {
+  const { email, password } = obj;
+  const user = await db.Users.findOne({
+    where: { email },
+  });
+  if (!user) {
+    throw "User with this email not found";
+  } else {
+    if (bcrypt.compareSync(password, user.password)) {
+      const token = jwt.sign({ user }, secret, { expiresIn: "1hr" });
+      return { user, token };
+    } else {
+      throw "Invalid password!!";
+    }
+  }
 };
 
 export const getAllUsers = async () => {
@@ -21,7 +43,7 @@ export const getAllUsers = async () => {
   return allUsers;
 };
 
-export const getUserById = async (id: any) => {
-  const user = await db.Users.findByPk(id);
+export const getUserById = async (email: any) => {
+  const user = await db.Users.findOne({ where: { email } });
   return user;
 };
