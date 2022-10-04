392a858b3445d5e795b6f6bf6b5c5c60a5f7cc71
ArmandoBGarcia <xdarcx@hotmail.es>
2022-10-04 12:14:59 -0500

    change rol for isAdmin

--- index.ts
+++ index.ts
@@ -12,7 +12,6 @@ async function getAnimes() {
 
     let animes: any = [];
     try {
-
       const resApi = await axios.get(`${url2}`);
       const resApi2 = await axios.get(`${url}`);
       const resApi3 = await axios.get(`${url3}`);
@@ -98,7 +97,6 @@ function preCarga() {
 }
 preCarga();
 getAnimes();
-
 
 const port = process.env.PORT || 3000;
 
