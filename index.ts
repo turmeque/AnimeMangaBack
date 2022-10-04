8bf5ac5a40e760ebfc4cc6424a8ecc93ad8000e6
https://github.com/turmeque <jhojangutierrez900@gmail.com>
2022-10-04 12:54:16 -0500

    changes

--- index.ts
+++ index.ts
@@ -139,7 +139,7 @@ async function getAnimes() {
 
 const port = process.env.PORT || 3000;
 
-db.sequelize.sync({ force: true }).then(async () => {
+db.sequelize.sync({ force: false}).then(async () => {
   app.listen(port, () => {
     console.log(`App listening on port ${port}`);
   });
