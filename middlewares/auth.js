df347d9f0de9d96a830a14c925141605c20e1a60
ArmandoBGarcia <xdarcx@hotmail.es>
2022-10-07 01:04:39 -0500

    some changes to impruve the functionality

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,25 +1,26 @@
 require("dotenv").config();
 const jwt = require("jsonwebtoken");
 const secret = process.env.SECRET_WORD;
+const { response, request } = require("express");
 import db from "../models";
 const auth = async (req = request, res = response, next) => {
   // console.log(req.headers);
 
   //check if exists the token
-  if (!req.headers.authorization) {
+  if (!req.headers.token) {
     res.status(401).json({ msg: "Access not autorized" });
   } else {
     //check the validation of the token
-    const token = req.headers.authorization.split(" ")[1];
-
-    console.log({ token });
+    const token = req.headers.token
 
     jwt.verify(token, secret, async (err, decoded) => {
       if (err) {
-        res.status(500).json({ msg: "Something went wrong", err });
+        // console.log(err);
+        res.status(500).json({ msg: "The session has expired, you must be logged", err });
+
       } else {
         const user = await db.Users.findByPk(decoded.user.id);
-        // console.log(user);
+        // console.log({user});
 
         if (!user) {
           return res
