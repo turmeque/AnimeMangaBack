7bbe636a3a3c5557cdcfad567718a9b4180a905c
ArmandoBGarcia <xdarcx@hotmail.es>
2022-10-11 12:03:52 -0500

    y

--- config/config.js
+++ config/config.js
@@ -13,14 +13,14 @@ module.exports = {
     username: "root",
     password: null,
     database: "database_test",
-    host: "127.0.0.1",
+    host: "0.0.0.0",
     dialect: "postgres",
   },
   production: {
     username: "root",
     password: null,
     database: "database_production",
-    
+
     host: "127.0.0.1",
     dialect: "postgres",
   },
