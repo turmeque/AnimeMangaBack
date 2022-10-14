c0b2eed5af2189e740ec684bf7f727ac5593f969
Zoen-DEV <enzoholgadodev@gmail.com>
2022-10-14 16:28:09 -0500

    nuevo modelo de reviews y agregando ruta delete al carro

--- src/routes/cart.routes.ts
+++ src/routes/cart.routes.ts
@@ -14,7 +14,7 @@ server.post("/", async (req, res) => {
       amount,
       totalPrice,
       UserId,
-      category, 
+      category,
     });
     res.send(response);
   } catch (error) {
@@ -27,7 +27,17 @@ server.get("/:userId", async (req, res) => {
   const { userId } = req.params;
   try {
     const response = await Cart.findAll({ where: { UserId: userId } });
-    res.status(200).json(response)
+    res.status(200).json(response);
+  } catch (err) {
+    console.error(err);
+  }
+});
+
+server.delete("/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const product = await Cart.destroy({ where: { id: id } });
+    res.send("Product deleted from cart")
   } catch (err) {
     console.error(err);
   }
