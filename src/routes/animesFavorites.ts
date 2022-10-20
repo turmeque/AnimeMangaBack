4f7ff98f2dc5e62efb52f874b3f1d105cf8e40d6
manolonolo <p.manolaki95@gmail.com>
2022-10-20 04:13:49 -0300

    get routes for anime/manga favorites

--- src/routes/animesFavorites.ts
+++ src/routes/animesFavorites.ts
@@ -1,8 +1,7 @@
 import {Router} from "express"
-import {
-  createAnimeFavorite, deleteAnimeFavorites
-    
-  } from "../controller/ControllerAnimesFavorites"
+import { createAnimeFavorite, deleteAnimeFavorites } from "../controller/ControllerAnimesFavorites";
+import db from "../../models";
+const { AnimeFavorites } = db;
 
 const server = Router();
 
@@ -16,6 +15,16 @@ server.post("/", async (req, res) => {
       res.status(200).json(newUser);
     } catch (error) {
       res.status(400).json({ msg: "Something went wrong", error });
+    }
+  });
+
+  server.get("/:userId", async (req, res) => {
+    const { userId } = req.params;
+    try {
+      const response = await AnimeFavorites.findAll({ where: { UserId: userId } });
+      res.status(200).json(response);
+    } catch (err) {
+      console.error(err);
     }
   });
   
--- src/routes/mangaFavorites.ts
+++ src/routes/mangaFavorites.ts
@@ -1,8 +1,8 @@
 import {Router} from "express"
-import {
-  createMangaFavorite, deleteMangaFavorites
-    
-  } from "../controller/controllerMangaFavorites";
+import { createMangaFavorite, deleteMangaFavorites } from "../controller/controllerMangaFavorites";
+
+import db from "../../models";
+const { MangaFavorites } = db;
 
 const server = Router();
 
@@ -10,12 +10,22 @@ const server = Router();
 
 server.post("/", async (req, res) => {
     
-    const { title,image,trailer,score,popularity,chapters,status,synopsis,genres,price,user  } = req.body;
+    const { title,image,score,popularity,chapters,status,synopsis,genres,price,user  } = req.body;
     try {
-      const newUser = await createMangaFavorite({  title,image,trailer,score,popularity,chapters,status,synopsis,genres,price,user });
+      const newUser = await createMangaFavorite({  title,image,score,popularity,chapters,status,synopsis,genres,price,user });
       res.status(200).json(newUser);
     } catch (error) {
       res.status(400).json({ msg: "Something went wrong", error });
+    }
+  });
+
+  server.get("/:userId", async (req, res) => {
+    const { userId } = req.params;
+    try {
+      const response = await MangaFavorites.findAll({ where: { UserId: userId } });
+      res.status(200).json(response);
+    } catch (err) {
+      console.error(err);
     }
   });
 
